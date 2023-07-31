0cdb0bef6c38b74fc75816b05892eca0f2f9ebd5
lipamaoa <filipa.ferreirasilva@gmail.com>
2023-07-31 22:40:51 +0100

    Solved Lab

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,21 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <div className="container">
+      <Link to={"/beers"}>
+        <img src="" alt="AllBears"/>
+        <h1>All Beers</h1>
+        </Link>
+      <Link to={"/beers/random"}>
+      <img src="" alt="RandomBears"/>
+        <h1>Random Beer</h1>
+        </Link>
+      <Link to={"/beers/new"}>
+      <img src="" alt="NewBears"/>
+        <h1>New Beer</h1></Link>
+    </div>
+  );
+}
 
 export default HomePage;
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,3 +1,39 @@
-function RandomBeersPage() {}
+import { useEffect, useState } from "react";
+
+const baseURL = "https://ih-beers-api2.herokuapp.com/beers/random";
+
+function RandomBeersPage() {
+  const [randomBeer, setRandomBeer] = useState(null);
+
+  const fetchRandomBeer = async () => {
+    try {
+      const response = await fetch(baseURL);
+      if (response.status === 200) {
+        const randomBeer = await response.json();
+        setRandomBeer(randomBeer);
+      }
+    } catch (error) {
+      console.error("Error fecthing random beer:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomBeer();
+  }, []);
+
+  return randomBeer ? (
+    <div>
+      <img src={randomBeer.image_url} alt={randomBeer.name} />
+      <h1>{randomBeer.name}</h1>
+      <h4>{randomBeer.tagline}</h4>
+      <p>First Brewed: {randomBeer.first_brewed}</p>
+      <p>Attenuation Level: {randomBeer.attenuation_level}</p>
+      <h5>{randomBeer.description}</h5>
+      <p>Contributed by: {randomBeer.contributed_by}</p>
+    </div>
+  ) : (
+    <h1>Loading...</h1>
+  );
+}
 
 export default RandomBeersPage;
